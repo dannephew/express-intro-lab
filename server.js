74d86cb2e86d040c98c070c2b6a2e15949442191
dannephew <danneszhang@gmail.com>
2021-07-13 14:15:44 -0700

    edit port number

--- server.js
+++ server.js
@@ -40,6 +40,6 @@ app.get("/cats", function(req, res) {
 
 // Tell the app to listen on port 3000
 
-app.listen(3001, function() {
-  console.log('Listening on port 3001')
+app.listen(3000, function() {
+  console.log('Listening on port 3000')
 })
